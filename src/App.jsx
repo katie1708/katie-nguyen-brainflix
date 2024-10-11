36010816a998c9cf29357459642f24724e9b42c3
katie1708 <katienguyen3012@gmail.com>
2024-10-11 07:51:07 -0700

    Add main video page and refactor project

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
-import Header from './components/Header.jsx'
-import Videoplayer from './components/Videoplayer.jsx'
-import VideoDetails from './components/VideoDetails/VideoDetails.jsx'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import MainVideoPage from './pages/MainVideoPage'
 
 function App() {
   return (
-    <>
-      <Header />
-      <Videoplayer />
-      <VideoDetails />
-    </>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainVideoPage />} />
+          <Route path="video/:videoId" element={<MainVideoPage />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/MainVideoPage.jsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header.jsx'
+import Videoplayer from '../components/Videoplayer.jsx'
+import VideoDetails from '../components/VideoDetails/VideoDetails.jsx'
+
+function MainVideoPage() {
+    return (
+        <>
+          <Header />
+          <Videoplayer />
+          <VideoDetails />
+        </>
+      )
+}
+
+export default MainVideoPage;
